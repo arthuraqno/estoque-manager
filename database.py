9658a802e344de35aeae94df367c90b5bf0027b5
Arthur Aquino <arthuraquino55@gmail.com>
2026-08-19 21:54:57 -0300

    feat: proteção de password do databse com variáveis de ambiente

--- database.py
+++ database.py
@@ -2,12 +2,16 @@ from sqlalchemy import create_engine
 from base import Base
 from models.produto import Produto
 from models.equipamento import Equipamento
+import os
+from dotenv import load_dotenv
 from models.suplemento import Suplemento
 from models.venda import Venda
 
-engine = create_engine(
-    "postgresql://postgres:104248652@localhost:5432/estoque_db"
-)
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_engine(DATABASE_URL)
 
 Base.metadata.create_all(engine)
 print("Tabelas criadas com sucesso!")
